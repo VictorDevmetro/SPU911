6ffd5f7a7d1456bb09c828c5afee56f1c24eb94a
Victor Matviiv <victor@barnyardcreative.com>
2021-12-04 12:37:27 +0200

    Razor part #2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 using SPU911.Models;
 using System;
@@ -13,6 +14,12 @@ namespace SPU911.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            ViewData["List"] = new List<string> { "first", "second", "third" };
+            base.OnActionExecuting(context);
+        }
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -20,7 +27,20 @@ namespace SPU911.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            ViewData["Title"] = "Custom title";
+            ViewData["Number"] = 1;
+
+            ViewBag.BagTitle = "Bag title";
+
+            var model = new HomeIndexImageModel
+            {
+                ImageUrl = "https://simple-fauna.ru/wp-content/uploads/2016/11/tukan-ptica_5.jpg",
+                ImageAltText = "Alternative text",
+                H = 150,
+                W = 240,
+                KByte = 12.5M
+            };
+            return View(model); 
         }
 
         /* /Home/Privacy */
@@ -30,13 +50,14 @@ namespace SPU911.Controllers
         //}
 
         /* /Home/Privacy/1 */
-        public IActionResult Privacy(int id)
+        public IActionResult Privacy(string id)
         {
             if (id == default)
             {
                 return View();
             }
-            return Ok(new { id = id});
+            ViewData["Number"] = id;
+            return View("PrivacyNumber");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/HomeIndexImageModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SPU911.Models
+{
+    public class HomeIndexImageModel
+    {
+        public string ImageUrl { get; set; }
+        public string ImageAltText { get; set; }
+        public int H { get; set; }
+        public int W { get; set; }
+        public decimal KByte { get; set; }
+
+        public override string ToString()
+        {
+            return ImageAltText;
+        }
+    }
+}
